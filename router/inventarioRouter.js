3a5eb185734c28d4a9102b369040dac5587cf514
JuanCastellanos89 <juan.castellanos@est.iudigital.edu.co>
2022-09-14 20:15:04 -0500

    Modificacion inventario

--- router/inventarioRouter.js
+++ router/inventarioRouter.js
@@ -1,12 +1,45 @@
 const { Router } = require('express');
+const Inventario = require('../models/Inventario');
 const router = Router();
 
-router.post('/', function(req, res){
-    res.send('Hola estoy en Inventario POST');
+router.post('/', async function(req, res){
+    try{
+        const existeInventarioSerial = await Inventario.findOne({ serial: req.body.serial});
+        if(existeInventarioSerial){
+            return res.send('Ya existe el serial para otro equipo!!!');
+        }
+        let inventario = new Inventario();
+        inventario.serial = req.body.serial;
+        inventario.modelo = req.body.modelo;
+        inventario.descripcion = req.body.descripcion;
+        inventario.foto = req.body.foto;
+        inventario.color = req.body.color;
+        inventario.fechaCompra = req.body.fechaCompra;
+        inventario.precio = req.body.precio;
+        inventario.usuario = req.body.usuario._id;
+        inventario.marca = req.body.marca._id;
+        inventario.tipoEquipo = req.body.tipoEquipo._id;
+        inventario.estadoEquipo = req.body.estadoEquipo._id;
+        inventario.fechaCreacion = new Date();
+        inventario.fechaActualizacion = new Date();
+        
+        inventario = await inventario.save();
+        res.send(inventario);
+    }catch(error){
+        console.log(error);
+        res.send('Error algo a fallado en Inventario');
+    }
+    
 });
 
-router.get('/', function(req, res){
-    res.send('Hola estoy en Inventario GET');
+router.get('/', async function(req, res){
+    try{
+        const inventarios = await Inventario.find();
+        res.send(inventarios);
+    }catch(error){
+        console.log(error);
+        res.send('Error algo a fallado en Inventario');
+    }
 });
 
 router.put('/', function(req, res){
