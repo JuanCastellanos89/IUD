b7efe0f6418149e81c290bf65546725359ece6fd
JuanCastellanos89 <juan.castellanos@est.iudigital.edu.co>
2022-09-15 12:07:36 -0500

    v0.3

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { getConnection } = require('./db/db-conecction-mongo');
 const cors = require('cors');
+require('dotenv').config();
 
 
 
-const port = 3000;
+const port = process.env.PORT;
 const app = express();
 app.use(cors());
 
