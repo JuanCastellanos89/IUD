2b47d9a7adca5363d93793c17356d7ad46920c2b
JuanCastellanos89 <113217035+JuanCastellanos89@users.noreply.github.com>
2022-09-20 23:54:23 -0500

    n/a

--- db/db-conecction-mongo.js
+++ db/db-conecction-mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const getConnection = async () => {
 
     try {
-        const url = 'mongodb://jcastellanosIUD:mxBxUqaakWg0KTYr@ac-wmuxz1p-shard-00-00.o3zyyhl.mongodb.net:27017,ac-wmuxz1p-shard-00-01.o3zyyhl.mongodb.net:27017,ac-wmuxz1p-shard-00-02.o3zyyhl.mongodb.net:27017/inventarios_iud?ssl=true&replicaSet=atlas-f15idu-shard-0&authSource=admin&retryWrites=true&w=majority';
+        const url = 'mongodb://jcastellanosIUD:fA5dzI48JzcmFfIj@ac-wmuxz1p-shard-00-00.o3zyyhl.mongodb.net:27017,ac-wmuxz1p-shard-00-01.o3zyyhl.mongodb.net:27017,ac-wmuxz1p-shard-00-02.o3zyyhl.mongodb.net:27017/inventarios_iud?ssl=true&replicaSet=atlas-f15idu-shard-0&authSource=admin&retryWrites=true&w=majority';
 
         await mongoose.connect(url);
 
