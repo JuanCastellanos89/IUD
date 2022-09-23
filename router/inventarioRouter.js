5788f15f03578e7a843072371df4899cbe094cbb
JuanCastellanos89 <113217035+JuanCastellanos89@users.noreply.github.com>
2022-09-23 11:39:06 -0500

    Update

--- router/inventarioRouter.js
+++ router/inventarioRouter.js
@@ -100,4 +100,18 @@ router.put('/:inventarioId', async function(req, res){
         res.status(500).send('Error algo a fallado en actualizar Inventario');
     }
 });
+
+router.get('/:inventarioId', async function(req, res) {
+    try{
+        const inventario = await Inventario.findById(req.params.inventarioId);
+        if(!inventario) {
+            return res.status(404).send('Inventario no Existe!!!');
+        } 
+        res.send(inventario);
+    }catch(error){
+        console.log(error);
+        res.status(500).send('Error algo a fallado en actualizar Inventario');
+    }
+});
+
 module.exports = router;
